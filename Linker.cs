980f26b7c6a76a6d7ffd3f3dade21fb53d259404
Alex Krikorian <akrikorian@envivent.com>
2019-01-14 17:08:18 -0800

    update: added code

---
+++ Linker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace EntranaDotNet.Helpers.Linker
+{
+  public static class Linker
+  {
+    private static Dictionary<string, string> Data;
+    static Linker()
+    {
+      using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("~/links.json")))
+      {
+        string json = reader.ReadToEnd();
+
+        Data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+      }
+    }
+    public static string GetUrl(string key)
+    {
+      return Data.ContainsKey(key) ? Data[key] : "#";
+    }
+  }
+}
